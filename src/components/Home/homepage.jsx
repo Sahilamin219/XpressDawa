52839652130c221a7d7154565177a787df83c260
SahilAmin <sahilamin219@gmail.com>
2021-01-06 07:09:31 +0530

    home started

--- src/components/Home/homepage.jsx
+++ src/components/Home/homepage.jsx
@@ -7,39 +7,115 @@ import { Link } from 'react-router-dom';
 const Homepage = (props) => {
 	return (
 	<>
-	<div className='row' style={{backgroundColor: "lightblue"}}>
-		// <div className='space'></div>
-		<div className="column">
+	<div style={{backgroundColor: "#E8E8E8"}}>
+	<div className='container' style={{backgroundColor: "lightblue"}}>
+		<div className="row">
 		<img src="pillpack.png" alt="logo" className='logo-h'/> 
 		</div>
-		<div class="column">
-		<p>by <b>XpressDawa</b> pharmacy</p>
+		<div class="row">
+		<p>&nbsp;&nbsp;by <b>XpressDawa</b> pharmacy</p>
 		</div>
 	</div>
 
-	<div className='hero-h'>
+	<div className='hero-h' style={{backgroundColor: "#E8E8E8"}}>
 		<h4>
 		Your medication,
 		<br/>
 		sorted and delivered 
 		</h4>
-		<p>PillPack is a full-service pharmacy</p>
-		<p>designed around your life.</p>
+		<p className='hero-p'>PillPack is a full-service pharmacy
+		</p><p className='hero-p'>
+		designed around your life.</p>
 		<p>&nbsp;&nbsp;</p>
-		<a class="btn btn-primary" href="#" role="button" style={{marginLeft :"3%"}}>Get Started</a>
+		<a className="btn btn-primary" href="#" role="button" style={{marginLeft :"3%"}}>Get Started</a>
 		<img src="pic0.png" class='rounded float-left' 	style={{marginLeft :"30%"}} width="80%" height="80%" alt="logo"/> 
 
 	</div>
 
+	<div className="hide-desktop hp-mobile-video" style={{backgroundColor: "white"}}>
+	<p>&nbsp;&nbsp;</p>
+	<p>Introducing <b>XpressDawa</b></p>
+		<p><b>Deliver them to you every month
+		<br/>
+		<a href="#" className='featured-link-txt'>See how it works</a></b></p>
+	</div>
+
+	<div className='.hp-meds-art-mod'>
+	<div className='section-inner'>
+	<p className='section-subh'>Full Service</p>
+	<h2 className='section-h'>A new kind of care</h2>
+	<div className="hp-meds-list-mod">
+		<div className="hp-meds-txt-item">
+			<h2 className='list-h'>Automatic refills</h2>
+
+			<p className='list-p'>We monitor and manage your refills with your doctors so you always have the medications you need.</p>
+		</div>
+	</div>
+	<div className="hp-meds-list-mod">
+		<div className="hp-meds-txt-item">
+			<h2 className='list-h'>Behind-the-scenes support</h2>
+
+			<p className='list-p'>We work directly with your doctors and insurance to save you time and hassle.</p>
+		</div>
+	</div>
+	<div className="hp-meds-list-mod">
+		<div className="hp-meds-txt-item">
+			<h2 className='list-h'>Everything in one place</h2>
+
+			<p className='list-p'>We organize all your medication, billing, and prescription details for easy access.</p>
+		</div>
+	</div>
+	</div>
+	</div>
+
+
+	 <form class="example">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search Medicines  "
+          />
+          <button type="submit">
+            <i class="fa fa-search"></i>
+          </button>
+        </form>
+        <div className="section">
+        <h3 className="centre">
+          <div className="space" />
+          I visit the pharmacy…
+        </h3>
+          <div class="card">
+            <h1 className="centre">
+              <i class="fas fa-calendar-check"></i>
+            </h1>
+            <p className="container-h">Every Week</p>
+          </div>
+          <div className="space" />
+          <div class="card">
+            <h1 className="centre">
+              <i class="fas fa-calendar-check"></i>
+            </h1>
+            <p className="container-h"> Once Or twice a month </p>
+          </div>
+          <div className="space" />
+          <div className="card">
+            <h1 className="centre">
+              <i class="fas fa-calendar-check"></i>
+            </h1>
+            <p className="container-h">Once in a while</p>
+          </div>
+	</div>
+
+
 
 	<div>
-		<div className="container">
+		<div className="footer-primary">
 		  <div className="row">
 		    <div className="span12">
 		      <div id="footer">
 		        <div className="row">
 		        <ul className='column'>
-		          <ul className='circle-btn' style={{backgroundColor: "lightblue"}} > <a href="#">Get Started</a></ul>
+		          <ul className='circle-btn' style={{backgroundColor: "lightblue"}} ><a href="#">Get Started</a></ul>
 		         </ul>
 		        <ul className='column'>
 		          <ul className='btn-outline'>999 010 8490</ul>
@@ -49,6 +125,7 @@ const Homepage = (props) => {
 		    </div>
 		  </div>
 		</div>
+	</div>
 	</div>
 	</>
 	);
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = (props) => {
 	      </li>
 	    </ul>
 	    <form class="form-inline my-2 my-lg-0">
-	      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+	      <input class="form-control mr-sm-2" type="search" placeholder="Search by medicine or brand name" aria-label="Search"/>
 	      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
 	    </form>
 	  </div>
